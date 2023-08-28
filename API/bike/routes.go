937fc9f04bdabff694f464e220a96e7f5965af14
rmsteja1 <teja.ramisetty@sjsu.edu>
2023-08-28 12:03:07 -0500

    Added routes for bike CRED operations

--- API/bike/routes.go
+++ API/bike/routes.go
@@ -9,11 +9,16 @@ import (
 )
 
 func Routes(r *mux.Router) {
-	// r := mux.NewRouter()
+	//routes for user
 	r.HandleFunc("/getAllUsers",getUsers).Methods("GET")
 	r.HandleFunc("/getOneUser/{id}",getUser).Methods("GET")
 	r.HandleFunc("/addOneUser",addUser).Methods("POST")
 	r.HandleFunc("/deleteOneUser/{email}",deleteUser).Methods("DELETE")
+
+	//routers for bikes
+	r.HandleFunc("/getAllBikes",getBikes).Methods("GET")
+	r.HandleFunc("/getOneBike/{id}",getBike).Methods("GET")
+	r.HandleFunc("/updateOneBike",updateBike).Methods("PUT")
 
 	fmt.Println("Starting server at 8080")
 	log.Fatal(http.ListenAndServe(":8080", r))
